225113f29ef7cdc6df2f78c8df492dd080895583
lia <gillia02@mail.ru>
2023-05-25 01:25:02 +0200

    add problem file

--- comment_delete.py
+++ comment_delete.py
@@ -1,3 +1,4 @@
+# тесты не падают
 @login_required
 def comment_delete(request, pk, comment_id):
     form = CommentForm
@@ -14,3 +15,16 @@ def comment_delete(request, pk, comment_id):
         'pk': pk,
     }
     return render(request, 'blog/detail.html', context)
+
+# тесты падают с ошибкой nonetype
+@login_required
+def comment_delete(request, pk, comment_id):
+    comment = get_object_or_404(Comment.objects.select_related('post'),
+                                id=comment_id, post__pk=pk)
+    if comment.author.username != request.user.username:
+        return redirect('blog:post_detail', pk=pk)
+    if request.method == 'POST':
+        comment.delete()
+    return redirect('blog:post_detail', pk=pk)
+
+
